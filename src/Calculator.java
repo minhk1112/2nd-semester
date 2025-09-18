32e7a017a1eaeb01a10d0a8e364574cf850ab180
Jeong Min Hyeok <wjdalsgur1234@cju.ac.kr>
2025-09-18 10:05:39 +0900

    java2 calculate

---
+++ src/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+    int num1;
+    int num2;
+    int result;
+
+    public void SetNumbers(int num1, int num2) {
+
+    }
+
+    public int add(){
+        return 0;
+    }
+
+    public int subtract(){
+        return 0;
+    }
+
+    public int multiply(){
+        return 0;
+    }
+
+    public int divide(){
+        return 0;
+    }
+
+    public void displayResult(){
+    }
+}
+
